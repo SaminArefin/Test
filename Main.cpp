1d36ccd2690bf0ab3afb946bffd3266491690562
Samin Arefin <nimas.inn@gmail.com>
2014-02-06 14:58:05 +0600

    Test.cpp added

---
+++ Main.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+ 
+#define foreach(x, v) for (typeof (v).begin() x=(v).begin(); x !=(v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define D(x) cout << #x " is " << x << endl
+#define REP(i,a,b) for (int i = int(a); i <= int(b); i++)
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+
+ 
+int main(){
+return 0;
+}
